11f6d4878778a8891fefb3e54f0a5f5b5a6e3549
Leon Zündel <leonzuendel1@gmail.com>
2020-12-17 03:48:39 +0100

    Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -87,8 +87,7 @@ export default {
     redirect: {
       login: "/user/login", // User will be redirected to this path if login is required
       logout: "/user/login", // User will be redirected to this path if after logout, current route is protected
-      home: "/", // User will be redirect to this path after login if accessed login page directly
-      callback: "/user/login"
+      home: "/" // User will be redirect to this path after login if accessed login page directly
     },
     rewriteRedirects: true
   },
